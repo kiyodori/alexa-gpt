54256ecf47d1dc447891ed86e46cf096fa9ef42b
kiyodori <kiyodori@yahoo.co.jp>
2025-02-27 15:54:05 +0900

    Update system message

--- lambda/keys.js
+++ lambda/keys.js
@@ -1,3 +1,3 @@
 module.exports.OPEN_AI_KEY = 'ここにAPIキーを入れる';
-module.exports.system_message = '小学生にもわかるように、短めで答えて。';
+module.exports.system_message = '200文字以内で、小学生にもわかるように簡単に答えてください。';
 module.exports.model = 'o1-mini';
